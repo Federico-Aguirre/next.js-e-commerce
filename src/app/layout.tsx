5c5ab1ea235a261c10d1c7fdcdde560f9c84cd62
Federico-Aguirre <sedent333@gmail.com>
2026-06-23 05:33:28 -0300

    levantando un entorno de tests(E2E) con Playwright

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Providers from "@/components/Providers"; // Importamos el proveedor
 import Navbar from "@/components/Navbar"; // O donde tengas tu Navbar instalado
 import CartSynchronizer from '@/components/CartSynchronizer';
 import WishlistSynchronizer from '@/components/WishlistSynchronizer';
